0ee35c77846fa99d9688d491c49c3f7f5d97af01
Kami <kami@kami.kami>
2025-09-21 22:21:22 +0300

    Initial commit - FPS 3D Project
    Former-commit-id: 081ff432ea11dd78358ac5ff4c5281252d777e88

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+ [SerializeField]
+ private float speed;
+
+ private PlayerMotor motor;
+ 
+ void Start()
+ {
+    motor = GetComponent<PlayerMotor>();
+ }
+ void Update()
+ {
+    //Vélocité vitesse mouvement du joueur
+    float xMov = Input.GetAxisRaw("Horizontal");//1 devan -1 arriere
+    float zMov = Input.GetAxisRaw("Vertical");
+
+    Vector3 moveHorizontal = transform.right * xMov;
+    Vector3 moveVertical = transform.forward * xMov; 
+
+    Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;//quelmle direction quelle vitesse
+
+ }
+}
