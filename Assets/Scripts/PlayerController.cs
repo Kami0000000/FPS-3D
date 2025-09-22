1a3b88fbcdae01f35638bc51a2d02cdc06e54ae0
Kami <kami@kami.kami>
2025-09-22 21:23:35 +0300

    Mouvement du joueur

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
  [SerializeField]
- private float speed;
+ private float speed = 3f;
+
+[SerializeField]
+ private float mouseSensitivityX = 3f;
+ [SerializeField]
+ private float mouseSensitivityY = 3f;
 
  private PlayerMotor motor;
  
@@ -19,9 +24,25 @@ public class PlayerController : MonoBehaviour
     float zMov = Input.GetAxisRaw("Vertical");
 
     Vector3 moveHorizontal = transform.right * xMov;
-    Vector3 moveVertical = transform.forward * xMov; 
+    Vector3 moveVertical = transform.forward * zMov; 
  
     Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;//quelmle direction quelle vitesse
+   motor.Move(velocity);
 
+   //Mouse x Mouse y Rotation du joueur
+   float yRot = Input.GetAxisRaw("Mouse X");
+
+
+   Vector3 rotation = new Vector3(0, yRot , 0) * mouseSensitivityX; //blocage de x et z 
+   motor.Rotate(rotation);
+
+
+
+    //Mouse x Mouse y Rotation du camera
+   float xRot = Input.GetAxisRaw("Mouse X");
+
+
+   Vector3 cameraRotation = new Vector3(xRot,0 , 0) * mouseSensitivityY; //blocage de x et z 
+   motor.RotateCamera(cameraRotation);
  }
 }
